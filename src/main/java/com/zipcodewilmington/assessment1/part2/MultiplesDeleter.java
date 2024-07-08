a3a032552c2c0a04060d51072f0eed7b5a57d914
Devi <diptikadevi@gmail.com>
2024-07-08 11:41:53 -0400

    completed p2.1 and p2.2

--- src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
@@ -1,5 +1,9 @@
 package com.zipcodewilmington.assessment1.part2;
 
+import org.junit.Assert;
+
+import java.util.ArrayList;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -10,7 +14,35 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with evens removed
      */
     public Integer[] deleteEvens(Integer[] ints) {
-        return null;
+//        ArrayList<Integer> deletedEvens = new ArrayList<>();
+//        for (int element : ints){
+//            if (element > ints.length-1){
+//                break;
+//            }
+//            else if (ints[element]%2 ==0){
+//                continue;
+//            } else {
+//                deletedEvens.add(ints[element]);
+//            }
+//        }
+//        return deletedEvens.toArray().toString();
+
+        int evenOccurence = 0;
+        for (int i : ints){
+            if (!(i % 2 == 0)){
+                evenOccurence++;
+                }
+        }
+        Integer[] removedEvens = new Integer[evenOccurence];
+        int i = -1;
+        for (int element : ints) {
+            if (!(element % 2 == 0)) {
+                removedEvens[i + 1] = element;
+                i++;
+
+            }
+        }
+        return removedEvens;
     }
 
     /**
@@ -19,7 +51,22 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with odds removed
      */
     public Integer[] deleteOdds(Integer[] ints) {
-        return null;
+        int oddOccurence = 0;
+        for (int i : ints){
+            if (i % 2 == 0) {
+                oddOccurence++;
+            }
+        }
+        Integer[] removedOdds = new Integer[oddOccurence];
+        int i = -1;
+        for (int element : ints) {
+            if (element % 2 == 0) {
+                removedOdds[i + 1] = element;
+                i++;
+
+            }
+        }
+        return removedOdds;
     }
 
     /**
@@ -28,7 +75,22 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by 3 removed
      */
     public Integer[] deleteMultiplesOf3(Integer[] ints) {
-        return null;
+        int noThreeMultOccurance = 0;
+        for (int i : ints){
+            if (i%3 != 0) {
+                noThreeMultOccurance++;
+            }
+        }
+        Integer[] removed3Multiples = new Integer[noThreeMultOccurance];
+        int i = -1;
+        for (int element : ints) {
+            if (element % 3 != 0) {
+                removed3Multiples[i + 1] = element;
+                i++;
+
+            }
+        }
+        return removed3Multiples;
     }
 
     /**
@@ -38,6 +100,22 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by `multiple` removed
      */
     public Integer[] deleteMultiplesOfN(Integer[] ints, int multiple) {
-        return null;
+        int nOccurance = 0;
+        for (int i : ints){
+            if (i % multiple == 0) {
+                nOccurance++;
+            }
+        }
+        Integer[] removedMultiples = new Integer[nOccurance];
+        int i = -1;
+        for (int element : ints) {
+            if (element % multiple != 0) {
+                removedMultiples[i + 1] = element;
+                i++;
+
+            }
+        }
+        return removedMultiples;
+
     }
 }
